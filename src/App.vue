cd9beb5a15a632e8b9226e919474ac77e4a4245d
Adam Marshall <adam@appsynergy.net>
2017-03-14 12:12:40 +0000

    Bootstrapping with style

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
-    <h1>Hello, world! ☺ - {{ foo }}</h1>
+    <div class="container">
+      <h1>Hello, world! ☺ - {{ foo }}</h1>
+    </div>
   </div>
 </template>
 
 <script lang="coffee">
-
   Component =
     name: 'app'
     data: () ->
       foo: "your content here"
 
   export default Component
-
 </script>
+
+<style lang="sass">
+  @import '../node_modules/bootstrap/dist/css/bootstrap.css'
+</style>
